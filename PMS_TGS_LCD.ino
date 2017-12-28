c57b4ab919d0cca6cbd30ff70e381bc685579c36
kalapontsai <kadelat@gmail.com>
2017-12-28 17:12:55 +0800

    Update PMS_TGS_LCD.ino
    LCD initial only, output will print out in serial monitor.

--- PMS_TGS_LCD.ino
+++ PMS_TGS_LCD.ino
@@ -2,15 +2,10 @@
 // LCD I2C Library，從這裡可以下載：
 // https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads
 #include <LiquidCrystal_I2C.h>
-const int buttonPin = 2;
-const int lcdtimer = 10;
-int buttonState = 0;  //開啟LCD螢幕燈的開關 A3
-int buttonAlive = 10;  //倒數10次
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(2, 3);
+SoftwareSerial mySerial(2, 3);//rx,tx
 // PMS5003T
-// http://jaihung.blogspot.tw/search/label/pms5003t
-#define PMS_SET 4   
+// code ref from http://jaihung.blogspot.tw/search/label/pms5003t
 long pmcf10=0;
 long pmcf25=0;
 long pmcf100=0;
@@ -23,7 +18,6 @@ long pm10PNO =0;
 long pm25PNO =0;
 long Temperature =0;
 long Humidity =0;
-
 char buf[50];
 
 // Set the pins on the I2C chip used for LCD connections:
@@ -59,95 +53,89 @@ void loop() {
   unsigned char c;
   unsigned char high;
   while (mySerial.available()) {
-  c = mySerial.read();
-  if((count==0 && c!=0x42) || (count==1 && c!=0x4d)){
-  Serial.println("check failed");
-  break;
-  }
-  if(count > 40){
-  Serial.println("complete");
-  break;
-  }
-  else if(count == 4 || count == 6 || count == 8 || count == 10 || count == 12 || count == 14|| count == 16|| count == 18|| count == 20|| count == 22|| count == 24|| count == 26|| count == 28 ) high = c;
-  else if(count == 5){
-  pmcf10 = 256*high + c;
-  Serial.print("CF=1, PM1.0=");
-  Serial.print(pmcf10);
-  Serial.println(" ug/m3");
-  }
-  else if(count == 7){
-  pmcf25 = 256*high + c;
-  Serial.print("CF=1, PM2.5=");
-  Serial.print(pmcf25);
-  Serial.println(" ug/m3");
-  }
-  else if(count == 9){
-  pmcf100 = 256*high + c;
-  Serial.print("CF=1, PM10=");
-  Serial.print(pmcf100);
-  Serial.println(" ug/m3");
-  }
-  else if(count == 11){
-  pmat10 = 256*high + c;
-  Serial.print("atmosphere, PM1.0=");
-  Serial.print(pmat10);
-  Serial.println(" ug/m3");
-  }
-  else if(count == 13){
-  pmat25 = 256*high + c;
-  Serial.print("atmosphere, PM2.5=");
-  Serial.print(pmat25);
-  Serial.println(" ug/m3");
-  }
-  else if(count == 15){
-  pmat100 = 256*high + c;
-  Serial.print("atmosphere, PM10=");
-  Serial.print(pmat100);
-  Serial.println(" ug/m3");
-  }
-  else if(count == 17){
-  pm03PNO= 256*high + c;
-  Serial.print("above0.3um, no=");
-  Serial.print(pm03PNO);
-  Serial.println(" number");
-  }
-  else if(count == 19){
-  pm05PNO = 256*high + c;
-  Serial.print("above0.5um, no=");
-  Serial.print(pm05PNO);
-  Serial.println(" number");
-  }
-  else if(count == 21){
-  pm10PNO = 256*high + c;
-  Serial.print("above 1.0um, no=");
-  Serial.print(pm10PNO);
-  Serial.println(" number");
-  }
-  else if(count == 23){
-  pm25PNO = 256*high + c;
-  Serial.print("above 2.5um, no=");
-  Serial.print(pm25PNO);
-  Serial.println(" number");
-  }
-  else if(count == 25){
-  Temperature = 256*high + c;
-  Serial.print("Temperature = ");
-  Serial.print(Temperature/10);
-  Serial.println(" ^C ");
-  }
-  else if(count == 27){
-  Humidity = 256*high + c;
-  Serial.print("Humidity = ");
-  Serial.print(Humidity/10) ;
-  Serial.println(" % ");
-  }
-  count++;
-  }
-  while(mySerial.available()) mySerial.read();
-  Serial.println();
-  u8g.firstPage();  
-      do {
-        draw();
-      } while( u8g.nextPage() );
+    c = mySerial.read();
+    if((count==0 && c!=0x42) || (count==1 && c!=0x4d)){
+      Serial.println("check failed");
+      break;
+      }
+    if(count > 40){
+      Serial.println("complete");
+      break;
+      }
+      else if(count == 4 || count == 6 || count == 8 || count == 10 || count == 12 || count == 14|| count == 16|| count == 18|| count == 20|| count == 22|| count == 24|| count == 26|| count == 28 ) high = c;
+      else if(count == 5){
+        pmcf10 = 256*high + c;
+        Serial.print("CF=1, PM1.0=");
+        Serial.print(pmcf10);
+        Serial.println(" ug/m3");
+        }
+      else if(count == 7){
+        pmcf25 = 256*high + c;
+        Serial.print("CF=1, PM2.5=");
+        Serial.print(pmcf25);
+        Serial.println(" ug/m3");
+        }
+      else if(count == 9){
+        pmcf100 = 256*high + c;
+        Serial.print("CF=1, PM10=");
+        Serial.print(pmcf100);
+        Serial.println(" ug/m3");
+        }
+      else if(count == 11){
+        pmat10 = 256*high + c;
+        Serial.print("atmosphere, PM1.0=");
+        Serial.print(pmat10);
+        Serial.println(" ug/m3");
+        }
+      else if(count == 13){
+        pmat25 = 256*high + c;
+        Serial.print("atmosphere, PM2.5=");
+        Serial.print(pmat25);
+        Serial.println(" ug/m3");
+        }
+      else if(count == 15){
+        pmat100 = 256*high + c;
+        Serial.print("atmosphere, PM10=");
+        Serial.print(pmat100);
+        Serial.println(" ug/m3");
+        }
+      else if(count == 17){
+        pm03PNO= 256*high + c;
+        Serial.print("above0.3um, no=");
+        Serial.print(pm03PNO);
+        Serial.println(" number");
+        }
+      else if(count == 19){
+        pm05PNO = 256*high + c;
+        Serial.print("above0.5um, no=");
+        Serial.print(pm05PNO);
+        Serial.println(" number");
+        }
+      else if(count == 21){
+        pm10PNO = 256*high + c;
+        Serial.print("above 1.0um, no=");
+        Serial.print(pm10PNO);
+        Serial.println(" number");
+        }
+      else if(count == 23){
+        pm25PNO = 256*high + c;
+        Serial.print("above 2.5um, no=");
+        Serial.print(pm25PNO);
+        Serial.println(" number");
+        }
+      else if(count == 25){
+        Temperature = 256*high + c;
+        Serial.print("Temperature = ");
+        Serial.print(Temperature/10);
+        Serial.println(" ^C ");
+        }
+      else if(count == 27){
+        Humidity = 256*high + c;
+        Serial.print("Humidity = ");
+        Serial.print(Humidity/10) ;
+        Serial.println(" % ");
+        }
+    count++;
+    }
   delay(4000);
 }
