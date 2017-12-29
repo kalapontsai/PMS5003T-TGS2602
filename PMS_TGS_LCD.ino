de25da95ee4c2ef46bb47ed74a291ee781f4f9e9
kalapontsai <kadelat@gmail.com>
2017-12-29 15:39:59 +0800

    Update PMS_TGS_LCD.ino

--- PMS_TGS_LCD.ino
+++ PMS_TGS_LCD.ino
@@ -4,8 +4,22 @@
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(2, 3);//rx,tx
-// PMS5003T
-// code ref from http://jaihung.blogspot.tw/search/label/pms5003t
+/* PlanTower PMS5003T
+code ref from http://jaihung.blogspot.tw/search/label/pms5003t
+Device pin:
+1:VCC (+5v)
+2:GND
+3:High (+3.3v) or Open is normal mode(Default), Low:Sleep mode
+4:RX (+3.3v)
+5:TX (+3.3v)
+6:RESET (+3.3v), Low:reset enable
+7:NC (eng purpose)
+8:NC (eng purpose)
+need to shift 5.5 to 3.3V between Arduino and PMS devices (use 1Kohm & 2Kohm R)
+*/
+
+//const int SET_Pin = 4;
+//const int ECO_Pin = 5; //Eco mode manual switch, Low = disable (Default)
 long pmcf10=0;
 long pmcf25=0;
 long pmcf100=0;
@@ -20,122 +34,147 @@ long Temperature =0;
 long Humidity =0;
 char buf[50];
 
+const int debug_mode = 1;
+
 // Set the pins on the I2C chip used for LCD connections:
 //                    addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // 設定 LCD I2C 位址
 
+void lcdmsg(int Yaxis,String msg){
+  lcd.setCursor(Yaxis,1);
+  lcd.print("                ");
+  lcd.setCursor(Yaxis,1);
+  lcd.print(msg);
+}
+
 void setup() {
-// put your setup code here, to run once:
   Serial.begin(9600);
   mySerial.begin(9600);
   pinMode(ledPower,OUTPUT);
-  lcd.begin(16, 2); // 初始化 LCD，一行 16 的字元，共 2 行，預設開啟背光
-    // 閃爍三次
-  for(int i=0; i<3; i++) {
-    lcd.backlight(); // 開啟背光
-    delay(250);
-    lcd.noBacklight(); // 關閉背光
-    delay(250);
+  digitalWrite(ledPower,0);
+  if (ECO_Pin) pinMode(Eco_Pin,INPUT);
+  if (SET_Pin) { pinMode(SET_Pin,OUTPUT);digitalWrite(SET_Pin,High);}
+  lcd.begin(16, 2); // LCD initial，16 words x 2 column，backlight ON
+  for(int i=0; i<3; i++) {  // shall be wait for 30 sec since internal fan power on
+    lcdmsg(0,"Humidity & Temp"); // to get the stable value
+    lcdmsg(1,"Dust PM2.5")
+    delay(1000);
+    lcd.clear();
+    delay(1000);
   }
-  lcd.backlight();
-  // 輸出初始化文字
-  lcd.setCursor(0, 0); // 設定游標位置在第一行行首
-  lcd.print("Humidity & Temp");
-  delay(1000);
-  lcd.setCursor(0, 1); // 設定游標位置在第二行行首
-  lcd.print("Dust PM2.5");
-  delay(500);
-  lcd.clear();
-}
 
 void loop() {
   int count = 0;
   unsigned char c;
   unsigned char high;
+  unsigned char c_all;
   while (mySerial.available()) {
+    digitalWrite(ledPower,1);
     c = mySerial.read();
+    c_all += c;
+    if(count > 40){
+      Serial.println("complete");
+      Serial.println("all string :");
+      Serial.println(c_all);
+      break;
+      }
     if((count==0 && c!=0x42) || (count==1 && c!=0x4d)){
       Serial.println("check failed");
       break;
       }
-    if(count > 40){
-      Serial.println("complete");
-      break;
+    else if(count == 4 || count == 6 || count == 8 || count == 10 || count == 12 || count == 14|| count == 16|| count == 18|| count == 20|| count == 22|| count == 24|| count == 26|| count == 28 ) high = c;
+    else if(count == 5){
+      pmcf10 = 256*high + c;
+      Serial.print("CF=1, PM1.0=");
+      Serial.print(pmcf10);
+      Serial.println(" ug/m3");
       }
-      else if(count == 4 || count == 6 || count == 8 || count == 10 || count == 12 || count == 14|| count == 16|| count == 18|| count == 20|| count == 22|| count == 24|| count == 26|| count == 28 ) high = c;
-      else if(count == 5){
-        pmcf10 = 256*high + c;
-        Serial.print("CF=1, PM1.0=");
-        Serial.print(pmcf10);
-        Serial.println(" ug/m3");
-        }
-      else if(count == 7){
-        pmcf25 = 256*high + c;
-        Serial.print("CF=1, PM2.5=");
-        Serial.print(pmcf25);
-        Serial.println(" ug/m3");
-        }
-      else if(count == 9){
-        pmcf100 = 256*high + c;
-        Serial.print("CF=1, PM10=");
-        Serial.print(pmcf100);
-        Serial.println(" ug/m3");
-        }
-      else if(count == 11){
-        pmat10 = 256*high + c;
-        Serial.print("atmosphere, PM1.0=");
-        Serial.print(pmat10);
-        Serial.println(" ug/m3");
-        }
-      else if(count == 13){
-        pmat25 = 256*high + c;
-        Serial.print("atmosphere, PM2.5=");
-        Serial.print(pmat25);
-        Serial.println(" ug/m3");
-        }
-      else if(count == 15){
-        pmat100 = 256*high + c;
-        Serial.print("atmosphere, PM10=");
-        Serial.print(pmat100);
-        Serial.println(" ug/m3");
-        }
-      else if(count == 17){
-        pm03PNO= 256*high + c;
-        Serial.print("above0.3um, no=");
-        Serial.print(pm03PNO);
-        Serial.println(" number");
-        }
-      else if(count == 19){
-        pm05PNO = 256*high + c;
-        Serial.print("above0.5um, no=");
-        Serial.print(pm05PNO);
-        Serial.println(" number");
-        }
-      else if(count == 21){
-        pm10PNO = 256*high + c;
-        Serial.print("above 1.0um, no=");
-        Serial.print(pm10PNO);
-        Serial.println(" number");
-        }
-      else if(count == 23){
-        pm25PNO = 256*high + c;
-        Serial.print("above 2.5um, no=");
-        Serial.print(pm25PNO);
-        Serial.println(" number");
-        }
-      else if(count == 25){
-        Temperature = 256*high + c;
-        Serial.print("Temperature = ");
-        Serial.print(Temperature/10);
-        Serial.println(" ^C ");
-        }
-      else if(count == 27){
-        Humidity = 256*high + c;
-        Serial.print("Humidity = ");
-        Serial.print(Humidity/10) ;
-        Serial.println(" % ");
-        }
+    else if(count == 7){
+      pmcf25 = 256*high + c;
+      Serial.print("CF=1, PM2.5=");
+      Serial.print(pmcf25);
+      Serial.println(" ug/m3");
+      }
+    else if(count == 9){
+      pmcf100 = 256*high + c;
+      Serial.print("CF=1, PM10=");
+      Serial.print(pmcf100);
+      Serial.println(" ug/m3");
+      }
+    else if(count == 11){
+      pmat10 = 256*high + c;
+      Serial.print("atmosphere, PM1.0=");
+      Serial.print(pmat10);
+      Serial.println(" ug/m3");
+      }
+    else if(count == 13){
+      pmat25 = 256*high + c;
+      Serial.print("atmosphere, PM2.5=");
+      Serial.print(pmat25);
+      Serial.println(" ug/m3");
+      }
+    else if(count == 15){
+      pmat100 = 256*high + c;
+      Serial.print("atmosphere, PM10=");
+      Serial.print(pmat100);
+      Serial.println(" ug/m3");
+      }
+    else if(count == 17){
+      pm03PNO= 256*high + c;
+      Serial.print("above0.3um, no=");
+      Serial.print(pm03PNO);
+      Serial.println(" number");
+      }
+    else if(count == 19){
+      pm05PNO = 256*high + c;
+      Serial.print("above0.5um, no=");
+      Serial.print(pm05PNO);
+      Serial.println(" number");
+      }
+    else if(count == 21){
+      pm10PNO = 256*high + c;
+      Serial.print("above 1.0um, no=");
+      Serial.print(pm10PNO);
+      Serial.println(" number");
+      }
+    else if(count == 23){
+      pm25PNO = 256*high + c;
+      Serial.print("above 2.5um, no=");
+      Serial.print(pm25PNO);
+      Serial.println(" number");
+      }
+    else if(count == 25){
+      Temperature = 256*high + c;
+      Serial.print("Temperature = ");
+      Serial.print(Temperature/10);
+      Serial.println(" ^C");
+      }
+    else if(count == 27){
+      Humidity = 256*high + c;
+      Serial.print("Humidity = ");
+      Serial.print(Humidity/10) ;
+      Serial.println(" %");
+      }
     count++;
     }
-  delay(4000);
+  lcd.backlight();
+  lcdmsg(0,"Temp     :" & Temperature/10);
+  lcdmsg(1,"Humidity :" & Humidity/10);
+  delay(1000);
+  lcdmsg(0,"PM 2.5 :" & pmat25);
+  lcdmsg(1,"PM 10  :" & pmat100);
+  delay(1000);
+  digitalWrite(ledPower,0);
+  delay(250);
+
+  if (Eco_Pin && pmat25 < 40) {  // 若節能模式有設jump及指數在正常範圍再檢查
+    int eco_mode = digitalRead(Eco_Pin);
+    if (eco_mode) {
+      lcd.noBacklight(); // 關閉背光
+      digitalWrite(SET_Pin,Low);
+      delay(60000);
+      digitalWrite(SET_Pin,High);
+      delay(60000);
+      }
+    }
 }
